cc9264feb19c086f9fdb5af7f00808254a2e407b
tayeb ahmed <tayebahmed@gmail.com>
2022-04-21 00:27:26 +0600

    create model Class for product

---
+++ lib/Models/product_model.dart
@@ -0,0 +1,10 @@
+class ProductModel {
+  String productName;
+  String productImage;
+  int productPrice;
+
+  ProductModel(
+      {required this.productName,
+      required this.productImage,
+      required this.productPrice});
+}
